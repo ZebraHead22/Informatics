df603c3dec106c6e139222283afe626067e5fd17
Maksim Baranov <baranovma1993@gmail.com>
2024-09-10 18:01:34 +0300

    del test file

--- my_python.py
+++
@@ -1,4 +0,0 @@
-import math
-x, t = 10, 1
-print(str(round(((9*math.pi*t + 10*math.cos(x)) * (math.exp(x))/(t**(1/2)-abs(math.sin(t)))), 2)),
-      "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla ")
