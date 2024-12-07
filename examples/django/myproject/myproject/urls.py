d1c1534c662d92891617d7d364286154248b9787
ZebraHead22 <baranovma1993@gmail.com>
2024-12-07 11:11:27 +0300

    homepage web app

--- examples/django/myproject/myproject/urls.py
+++ examples/django/myproject/myproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),  # Подключаем приложение
+    path('', include('homepage.urls')),  # Подключаем приложение
 ]
